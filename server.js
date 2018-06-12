998e9a88ad4d8ae081f5a2eb68de6496f480f201
Chineze Nwosu <chineze.nwosu@andela.com>
2018-06-12 17:19:07 +0100

    implement message handling

--- server.js
+++ server.js
@@ -9,13 +9,27 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(router)
 app.get("/", verificationHandler)
+app.post("/", handleMessage);
 
 function verificationHandler(req, res) {
   console.log(req)
-  if (req.query['hub.verify_token'] === 'verifycode') {
-    res.send(req.query['hub.challenge']);
+  if (req.query["hub.verify_token"] === "verifycode") {
+    res.send(req.query["hub.challenge"]);
   }
-  res.send('Error, wrong validation token!');
+  res.send("Error, wrong validation token!");
+}
+
+function handleMessage(req, res) {
+  var messagingEvents = req.body.entry[0].messaging;
+  for (i = 0; i < messagingEvents.length; i++) {
+    event = req.body.entry[0].messaging[i];
+
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(text);
+    }
+  }
+  res.send("received!");
 }
 
 http.createServer(app).listen(app.get("port"), function() {
